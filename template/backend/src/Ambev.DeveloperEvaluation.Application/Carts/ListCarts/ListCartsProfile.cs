5c91f915e91919e78f5f76b021efd188ac8c368d
Bruno <brun.op@hotmail.com>
2025-08-06 14:00:50 -0300

    feat(carts): list carts

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Carts/ListCarts/ListCartsProfile.cs
@@ -0,0 +1,20 @@
+﻿using Ambev.DeveloperEvaluation.Domain.ValueObjects.Carts;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.Application.Carts.ListCarts;
+
+/// <summary>
+/// Profile for mapping between Cart entity and ListCartsResult
+/// </summary>
+public class ListCartsProfile : Profile
+{
+    /// <summary>
+    /// Initializes the mappings for ListCarts operation
+    /// </summary>
+    public ListCartsProfile()
+    {
+        CreateMap<Domain.Entities.Cart, CartVo>();
+        CreateMap<PaginatedListCart, ListCartsResult>()
+            .ForMember(dest => dest.Data, opt => opt.MapFrom(src => src.Data));
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Carts/ListCarts/ListCartsRequest.cs
@@ -0,0 +1,9 @@
+﻿using Ambev.DeveloperEvaluation.WebApi.Common;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Carts.ListCarts;
+
+/// <summary>
+/// Query for retrieve carts by params
+/// </summary>
+public class ListCartsRequest : QueryListFiltered
+{}
